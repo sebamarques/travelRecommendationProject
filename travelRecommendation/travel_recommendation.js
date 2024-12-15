c18feef869771fa210bb9dd7d58b1726c26521ed
sebamarques <sebamarquezblanco@gmail.com>
2024-12-16 00:41:03 +0100

    logica buscador

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -22,10 +22,46 @@ document.addEventListener("DOMContentLoaded",function(){
 
     peoples.forEach(people =>{
         agregarContenido += `
-            <h2>${people.name}</h2>
-            <p>${people.position}</p>
+            <div class="ourTeamPeople">
+                <p class="ourTeam__name">${people.name}</p>
+                <p class="common">${people.name} is responsible for...</p>
+                <p class="ourTeam__position">${people.position}</p>
+            </div>
+            
         `
     })
     equipo.innerHTML = agregarContenido;
-    agregarContenido = "";
+    
 })
+
+
+function borrarContenido(){
+    document.getElementById("buscador").value = "";
+    let mostrar = document.getElementById('resultadoBusqueda')
+    mostrar.innerHTML = ""
+}
+
+function buscar(){
+    let busqueda = document.getElementById('buscador').value.toLowerCase();
+    let mostrar = document.getElementById('resultadoBusqueda')
+    let agregarContenido = ""
+    fetch("/travelRecommendation/travel_recommendation_api.json")
+    .then(response => response.json())
+    .then(data =>{
+        let keys = Object.keys(data)
+        keys.forEach(key => {
+            if (key.includes(busqueda)) {
+                data[key].forEach(valor => {
+                    agregarContenido += `
+                    <img src="${valor.imageUrl}">
+                    <p>${valor.name}</p>
+                    <p>${valor.description}</p>
+                    `;
+                });
+                mostrar.innerHTML = agregarContenido;
+                agregarContenido = "";
+            }
+        })        
+    })
+
+}
